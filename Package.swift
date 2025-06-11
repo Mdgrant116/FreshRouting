f2f45ba5b32776da1f946c1c9314b9d8d0f0c3d6
Michael Grant <mdgrant116@gmail.com>
2025-06-11 18:09:42 -0400

    Updated

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FreshRouting",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v10_15)
+        .macOS(.v12), .iOS(.v16), .tvOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
